bacd6aa56960cc7a3e45e358ef7a9da97a115180
Matthew <matthewdemetriades@Matthews-MacBook-Pro.local>
2024-08-26 17:15:49 +0100

    Removed duplicate file after renaming.

--- Assignments/obj/Debug/net8.0/Assignments.AssemblyInfo.cs
+++ Assignments/obj/Debug/net8.0/Assignments.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignments")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2699a690f6307eb9a9427c9767f11aaa20803b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignments")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignments")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
